751044ef7cc84039ce284990b84d12d27567cbc4
PH-KDX <smtp.python.email.sender@gmail.com>
2020-08-30 14:07:04 +0200

    Edited readme, added new features decode and search (as of yet undocumented)

--- flightplandb.py
+++ flightplandb.py
@@ -4,7 +4,7 @@ from requests.auth import HTTPBasicAuth
 
 class API:
 
-    """Checks API status to see if it is up"""
+    # Checks API status to see if it is up
     @staticmethod
     def ping(key):
         url = "https://api.flightplandatabase.com/"
@@ -14,7 +14,7 @@ class API:
         # the reponse gets passed back as JSON
         return(result.headers, result.json())
 
-    """Revokes API key"""
+    # Revokes API key
     @staticmethod
     def revoke(key):
         url = "https://api.flightplandatabase.com/auth/revoke"
@@ -25,7 +25,7 @@ class API:
 # General actions pertaining to flight plans
 class Plan:
 
-    """Fetches a flight plan and its associated route by ID"""
+    # Fetches a flight plan and its associated route by ID
     @staticmethod
     def fetch(key, id):
         if not isinstance(id, int):
@@ -35,16 +35,16 @@ class Plan:
             result = requests.get(url, auth=HTTPBasicAuth(key, None))
             return(result.headers, result.json())
 
-    """Takes a dict and posts it to a flight plan"""
+    # Takes a dict and posts it to a flight plan
     @staticmethod
     def post(key, route):
         url = "https://api.flightplandatabase.com/plan"
         result = requests.post(url, json=route, auth=HTTPBasicAuth(key, None))
         return(result.headers, result.json())
 
-    """Takes a dict and updates an existing flight plan based on it"""
+    # Takes a dict and updates an existing flight plan based on it
     @staticmethod
-    def patch(key, id, route):
+    def edit(key, id, route):
         if not isinstance(id, int):
             raise ValueError("The flight plan ID must be an integer")
         else:
@@ -56,14 +56,7 @@ class Plan:
             )
             return(result.headers, result.json())
 
-    """Creates a new flight plan using the route generator"""
-    @staticmethod
-    def generate(key, params):
-        url = "https://api.flightplandatabase.com/auto/generate"
-        result = requests.post(url, json=params, auth=HTTPBasicAuth(key, None))
-        return(result.headers, result.json())
-
-    """Deletes a flight plan and its associated route by ID"""
+    # Deletes a flight plan and its associated route by ID
     @staticmethod
     def delete(key, id):
         if not isinstance(id, int):
@@ -73,18 +66,44 @@ class Plan:
             result = requests.delete(url, auth=HTTPBasicAuth(key, None))
             return(result.headers, result.json())
 
+    # Creates a new flight plan using the route generator
+    @staticmethod
+    def generate(key, params):
+        url = "https://api.flightplandatabase.com/auto/generate"
+        result = requests.post(url, json=params, auth=HTTPBasicAuth(key, None))
+        return(result.headers, result.json())
+
+    # Converts a route from a space-separated string to a flight plan
+    @staticmethod
+    def decode(key, route):
+        url = "https://api.flightplandatabase.com/auto/decode"
+        route_dict = {"route": route}
+        result = requests.post(
+            url,
+            json=route_dict,
+            auth=HTTPBasicAuth(key, None)
+            )
+        return(result.headers, result.json())
+
+    # Searches for a route based on several parameters
+    @staticmethod
+    def search(key, params):
+        url = "https://api.flightplandatabase.com/search/plans"
+        result = requests.post(url, json=params, auth=HTTPBasicAuth(key, None))
+        return(result.headers, result.json())
+
 
 # Fetches various info related to airports
 class Airport:
 
-    """Fetches info about airport by ICAO code"""
+    # Fetches info about airport by ICAO code
     @staticmethod
     def info(key, icao):
         url = f"https://api.flightplandatabase.com/nav/airport/{icao}"
         result = requests.get(url, auth=HTTPBasicAuth(key, None))
         return(result.headers, result.json())
 
-    """Fetches weather for airport by ICAO code"""
+    # Fetches weather for airport by ICAO code
     @staticmethod
     def weather(key, icao):
         url = f"https://api.flightplandatabase.com/weather/{icao}"

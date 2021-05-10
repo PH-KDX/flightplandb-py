75a9a6bf2da120bcda47349bde9990e4e3fc827d
PH-KDX <smtp.python.email.sender@gmail.com>
2021-05-10 19:35:15 +0200

    updated setup.py in preparation for pip

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="flightplandb",
     version="0.3.0",
     author="PH-KDX",
     author_email="smtp.python.email.sender@gmail.com",
     url="https://github.com/PH-KDX/flightplandb-py/",
+    project_urls={
+        "Documentation": "https://flightplandb-py.readthedocs.io/",
+        "Issue tracker": "https://github.com/PH-KDX/flightplandb-py/issues",
+    },
+    description="Python wrapper for the Flight Plan Database API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "requests==2.25.1",
@@ -17,5 +27,20 @@ setup(
             "Sphinx==4.0.0",
             "sphinx-rtd-theme==0.5.2"
         ]
-    }
+    },
+    python_requires='>=3.7.0',
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Topic :: Games/Entertainment :: Simulation"
+    ]
 )

9cdc5fb3ebd15a42582d166a72aac8c8dba8a768
PH-KDX <smtp.python.email.sender@gmail.com>
2021-05-10 22:33:36 +0200

    added release workflow and worked on setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "requests==2.25.1",
         "python-dateutil==2.8.1"

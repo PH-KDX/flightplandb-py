c10b26847d4aafbf61a126fafb177b2012876e69
PH-KDX <smtp.python.email.sender@gmail.com>
2021-05-25 13:15:21 +0200

    updated version in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, PH-KDX'
 author = 'PH-KDX'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.3'
 
 
 # -- General configuration ---------------------------------------------------

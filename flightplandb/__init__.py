02e2559cb222ca420e3735491b8886e3aed028ae
PH-KDX <smtp.python.email.sender@gmail.com>
2021-10-10 11:41:25 +0200

    bump package patch version

--- flightplandb/__init__.py
+++ flightplandb/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Version of the flightplandb package
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 from flightplandb.flightplandb import *  # noqa: F403, F401
 from flightplandb.datatypes import *  # noqa: F403, F401

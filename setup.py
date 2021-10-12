e73e55735b361508a206f87b985ea12c255eb786
PH-KDX <smtp.python.email.sender@gmail.com>
2021-10-12 18:11:01 +0200

    stop flake8 from complaining

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     description="Python wrapper for the Flight Plan Database API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir={"":"src"},
+    package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
     install_requires=[

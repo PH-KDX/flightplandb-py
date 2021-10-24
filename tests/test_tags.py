4952bc0c6263ebf8f9a99fb384d253ae3077c3fa
PH-KDX <smtp.python.email.sender@gmail.com>
2021-10-24 15:31:08 +0200

    convert tags and weather tests to pytest

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -4,18 +4,18 @@ from flightplandb.datatypes import Tag
 
 def test_tags_api(mocker):
     json_response = [
-                        {
-                            "name": "Decoded",
-                            "description": "Flight plans decoded",
-                            "planCount": 7430,
-                            "popularity": 0.010143822356129395
-                        },
-                        {
-                            "name": "Generated",
-                            "description": "Computer generated plans",
-                            "planCount": 35343,
-                            "popularity": 0.009036140132228622
-                        }
+        {
+            "name": "Decoded",
+            "description": "Flight plans decoded",
+            "planCount": 7430,
+            "popularity": 0.010143822356129395
+        },
+        {
+            "name": "Generated",
+            "description": "Computer generated plans",
+            "planCount": 35343,
+            "popularity": 0.009036140132228622
+        }
                     ]
 
     correct_response = [
@@ -26,7 +26,8 @@ def test_tags_api(mocker):
         Tag(name='Generated',
             description='Computer generated plans',
             planCount=35343,
-            popularity=0.009036140132228622)]
+            popularity=0.009036140132228622)
+        ]
 
     def patched_get(self, path, key):
         return json_response

f6cfe2c3b9f3232af0ee7c5e66561836e2c10c62
PH-KDX <smtp.python.email.sender@gmail.com>
2022-05-02 20:57:10 +0200

    clean up __init__.py

--- src/flightplandb/__init__.py
+++ src/flightplandb/__init__.py
@@ -5,6 +5,4 @@ __version__ = "0.5.0"
 
 from . import internal, exceptions, datatypes, submodules
 
-# from flightplandb.datatypes import *  # noqa: F403, F401
-# from flightplandb.submodules import *  # noqa: F403, F401
 __all__ = ["internal", "exceptions", "datatypes", "submodules"]

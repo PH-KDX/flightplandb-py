7923b4c67b1df7034023b8573d2dd62f0a9b66a0
PH-KDX <smtp.python.email.sender@gmail.com>
2022-08-15 22:40:02 +0200

    convert tags functions to async

--- src/flightplandb/tags.py
+++ src/flightplandb/tags.py
@@ -4,7 +4,7 @@ from flightplandb.datatypes import Tag
 from flightplandb import internal
 
 
-def fetch(key: Optional[str] = None) -> List[Tag]:
+async def fetch(key: Optional[str] = None) -> List[Tag]:
     """Fetches current popular tags from all flight plans.
     Only tags with sufficient popularity are included.
 
@@ -19,4 +19,6 @@ def fetch(key: Optional[str] = None) -> List[Tag]:
         A list of the current popular tags.
     """
 
-    return list(map(lambda t: Tag(**t), internal.get(path="/tags", key=key)))
+    return list(
+        map(lambda t: Tag(**t), await internal.get(path="/tags", key=key))
+    )

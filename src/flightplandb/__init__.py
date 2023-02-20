9e9d95cd1bb0b32a7ccafbebfb31b6013b4235fe
PH-KDX <smtp.python.email.sender@gmail.com>
2023-02-20 01:31:46 +0100

    bump package version

--- src/flightplandb/__init__.py
+++ src/flightplandb/__init__.py
@@ -10,7 +10,7 @@ library, read the documentation at https://flightplandb-py.readthedocs.io/.
 
 
 # Version of the flightplandb package
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from . import (
     internal, exceptions, datatypes,

12f75ae18e0a07da0ed8e4a88e9b3d8fce3a6062
PH-KDX <smtp.python.email.sender@gmail.com>
2023-05-04 00:48:25 +0200

    run black and isort on sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,15 +11,17 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
+
 from flightplandb import __version__
-sys.path.insert(0, os.path.abspath('../../'))
+
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'FlightplanDB-py'
-copyright = '2022, PH-KDX'
-author = 'PH-KDX'
+project = "FlightplanDB-py"
+copyright = "2022, PH-KDX"
+author = "PH-KDX"
 
 # The full version, including alpha/beta/rc tags
 release = __version__
@@ -33,22 +35,23 @@ version = release
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['sphinx.ext.autodoc',  # HTML generation from docstrings
-              'sphinx.ext.napoleon',  # Read NumPy style docstrings
-              'sphinx.ext.autosummary',  # Create neat summary tables
-              ]
+extensions = [
+    "sphinx.ext.autodoc",  # HTML generation from docstrings
+    "sphinx.ext.napoleon",  # Read NumPy style docstrings
+    "sphinx.ext.autosummary",  # Create neat summary tables
+]
 
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -56,7 +59,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -70,11 +73,11 @@ html_favicon = "../../artwork/ico/favicon.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 smartquotes = False
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 
 def setup(app):

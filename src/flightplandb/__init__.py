8dcb2a2ae1d7703d032a93fea02080e0fcea3f49
PH-KDX <smtp.python.email.sender@gmail.com>
2023-12-24 01:02:27 +0100

    bump version number in __init__.py

--- src/flightplandb/__init__.py
+++ src/flightplandb/__init__.py
@@ -10,7 +10,7 @@ library, read the documentation at https://flightplandb-py.readthedocs.io/.
 
 
 # Version of the flightplandb package
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 from . import api, datatypes, exceptions, internal, nav, plan, tags, user, weather
 
